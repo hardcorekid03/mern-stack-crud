d7a4c0bb667cbd89ed5cf6098b146d81de716a41
hardcorekid03 <parasapremiumko88@gmail.com>
2024-05-31 11:36:40 +0800

    add useNavigate to homepage

--- client/src/pages/Login/Login.jsx
+++ client/src/pages/Login/Login.jsx
@@ -8,10 +8,11 @@ function Login() {
   const [email, setEmail] = useState('')
   const [password,setPassword] = useState('')
   const {login, error, isLoading} = useLogin()
-
+  
   const handleSubmit = async (e) => {
     e.preventDefault()
-    await login(email,password)    }
+    await login(email, password)
+  }
 
   return (
     <>
